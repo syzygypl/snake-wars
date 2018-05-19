7ae71e722ba1e4723ad34fc2490bf29a02cbe22c
Maciej Łebkowski <m.lebkowski@gmail.com>
2018-05-19 11:33:52 +0200

    fix backend path

--- frontend/src/components/Scoreboard/Scoreboard.js
+++ frontend/src/components/Scoreboard/Scoreboard.js
@@ -2,10 +2,6 @@ import React, { Component } from 'react';
 import './Scoreboard.css';
 
 export default class Scoreboard extends Component {
-    constructor(props) {
-        super(props);
-    }
-
     render() {
         return (
             <div className="scoreboard">
